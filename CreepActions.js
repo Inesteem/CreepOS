7a6b38fdf4ba9cf48e48fb18a26ee2e19ccaeda2
Lucifer Hemp <lucifer.hemp@fau.de>
2020-11-17 21:36:20 +0100

    miris code

---
+++ CreepActions.js
@@ -0,0 +1,32 @@
+
+Creep.prototype.harvestClosest = function (){
+    const target = this.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+    if(target && this.harvest(target) == ERR_NOT_IN_RANGE) {
+        this.moveTo(target, {visualizePathStyle: {stroke: '#ffff00'}});
+    }
+}
+
+Creep.prototype.harvestFrom = function (target){
+    if(target && this.harvest(target) == ERR_NOT_IN_RANGE) {
+        this.moveTo(target, {visualizePathStyle: {stroke: '#ffff00'}});
+    }
+}
+
+Creep.prototype.storeAt = function(structure) {
+    if(this.transfer(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+        this.moveTo(structure, {visualizePathStyle: {stroke: '#ff00ff'}});
+    }
+}
+
+Creep.prototype.upgrade = function() {
+    if(this.upgradeController(this.room.controller) == ERR_NOT_IN_RANGE) {
+        this.moveTo(this.room.controller, {visualizePathStyle: {stroke: '#00ffff'}});
+    }
+}
+
+Creep.prototype.buildStructure = function(structure) {
+    if(this.build(structure) == ERR_NOT_IN_RANGE) {
+        this.moveTo(structure, {visualizePathStyle: {stroke: '#0000ff'}});
+    }
+}
+
--- main.js
+++ main.js
@@ -1,53 +1,122 @@
-var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
-var roleBuilder = require('role.builder');
+var creep_actions = require("CreepActions");
 
-var spawnMachine = require('spawnMachine');
+var harvest_id = "harvesting";
+var upgrade_id = "upgrading";
+var store_id = "storing";
+var build_id = "building";
 
-if (!Creep.prototype.harvestNearest) {
-    console.log("new function");
-    Creep.prototype.harvestNearest = function() {
-        var src = this.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
-        if(this.harvest(src) == ERR_NOT_IN_RANGE) {
-            this.moveTo(src, {visualizePathStyle: {stroke: '#ffaa00'}});
-        }
-    };
-} 
 
-var workers = [
-{"name":'harvester',    "num":3,"body":[WORK,CARRY,MOVE,MOVE,MOVE,MOVE]},
-{"name":'builder',      "num":3,"body":[WORK,CARRY,MOVE,MOVE,MOVE,MOVE]},
-{"name":'upgrader',     "num":6,"body":[WORK,CARRY,MOVE,MOVE,MOVE,MOVE]}
-];
+var fillSpawn = function(creep) {
+    if (!creep.memory.state){
+        creep.memory.state = harvest_id;
+        creep.memory.source_id = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE).id;
+    }
+    if (creep.memory.state == harvest_id && creep.store.getFreeCapacity() == 0) {
+        creep.memory.state = store_id;
+    } else if (creep.memory.state == store_id && 
+            (creep.store[RESOURCE_ENERGY] == 0 ||
+             Game.spawns['Spawn1'].store.getFreeCapacity(RESOURCE_ENERGY)== 0)) {
+        creep.memory.source_id = null;
+        creep.memory.state = null;
+        return false;
+    }
 
-module.exports.loop = function () {
+    if(creep.memory.state == harvest_id ) {
+        if (!creep.memory.source_id)
+            creep.memory.source_id = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE).id;
+        creep.harvestFrom(Game.getObjectById(creep.memory.source_id));
+    }
+    else {
+        var spawn = Game.spawns["Spawn1"];
+        creep.storeAt(spawn);
+    }   
+    return true;
+}
 
+var upgradeController = function (creep){
+    if(!creep.memory.state) creep.memory.state = harvest_id;
+    if (creep.memory.state == harvest_id && creep.store.getFreeCapacity() == 0)
+        creep.memory.state = upgrade_id;
+
+    else if (creep.memory.state == upgrade_id  && creep.store[RESOURCE_ENERGY] == 0){
+        creep.memory.state = null;
+        return false;
+    }
+
+    if(creep.memory.state == harvest_id ) {
+        creep.harvestClosest();
+    }
+    else {
+        creep.upgrade();
+    }   
+    return true;
+}
+
+var buildClosest = function (creep){
     for(var name in Memory.creeps) {
         if(!Game.creeps[name]) {
-            delete Memory.creeps[name];
-            console.log('Clearing non-existing creep memory:', name);
+            Game.spawns['Spawn1'].spawnCreep([WORK,CARRY, MOVE], Memory.creeps[name]);
         }
     }
 
-
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
-            roleHarvester.run(creep);
-        }
-        if(creep.memory.role == 'upgrader') {
-            roleUpgrader.run(creep);
-        }
-        if(creep.memory.role == 'builder') {
-            roleBuilder.run(creep);
-        }
+    if(!creep.memory.state) creep.memory.state = harvest_id;
+    if (creep.memory.state == harvest_id && creep.store.getFreeCapacity() == 0) {
+        creep.memory.structure_id = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES).id;
+        creep.memory.state = build_id;
+    }
+    if (creep.memory.state == build_id  && 
+            (!creep.memory.structure_id || creep.store[RESOURCE_ENERGY] == 0
+             || Game.getObjectById(creep.memory.structure_id).progress ==
+             Game.getObjectById(creep.memory.structure_id).progressTotal)){
+        creep.memory.state = null;
+        creep.memory.structure_id = null;
+        return false;
     }
 
-    for (var i in workers) {
-        spawnMachine.spawnWorker(workers[i].name, workers[i].num, workers[i].body);
+    if(creep.memory.state == harvest_id ) {
+        creep.harvestClosest();
     }
+    else {
+        if (!creep.memory.structure_id)
+            creep.memory.structure_id = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES).id;
+        creep.buildStructure(Game.getObjectById(creep.memory.structure_id));
+    }   
+    return true;
 }
 
 
+module.exports.loop = function () {
+    console.log(JSON. stringify(Memory.tasks));
+    for(var name in Memory.creeps) {
+        if(!Game.creeps[name]) {
+            if (Memory.creeps[name].task)
+                Memory.tasks.push(Memory.creeps[name].task);
+            delete Memory.creeps[name];
+        }
+    }
+
+    if (_.filter(Game.creeps, (creep) => true).length < 7) {
+        var newName = "Kevin" + Game.time;
+        Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], newName);
+    }
+
+    var task_mapping = {
+        'fill_spawn': fillSpawn,
+        'upgrade': upgradeController,
+        'build': buildClosest
+    };
+
+    _.forEach(Game.creeps, (creep) => {
+        if (!creep.memory.task) {
+            creep.memory.task = Memory.tasks.shift();
+        } else {
+            if(!task_mapping[creep.memory.task](creep)){
+                Memory.tasks.push(creep.memory.task);
+                creep.memory.task = null;
+            }
+        }
+    });
+
+}
 
 
